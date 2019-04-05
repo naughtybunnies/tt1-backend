3f5c95dc2794ba313f488cf83752b7a503549845
tonny <ton_kriddanai@hotmail.com>
2019-04-05 15:37:20 +0700

    format into pep-8 (with beautiful assignment)

--- src/datatable.py
+++ src/datatable.py
@@ -1,16 +1,17 @@
 import re
 import os
 
-from src.config import PATH_REPO,PATH_SCRAPED,PATH_PARSED
+from src.config import PATH_REPO, PATH_SCRAPED, PATH_PARSED
 from src.graph import Graph
+
 
 class DataTable(object):
     def __init__(self):
         self.data_table = {}
-    
+
     def add_column(self, column, xpath):
         self.data_table[column] = xpath
-    
+
     def del_column(self, column):
         self.data_table.pop(column)
 
@@ -24,22 +25,25 @@ class DataTable(object):
             for item in from_body.split("/")[1:]:
                 result = re.match(pattern, item)
                 if(result is not None):
-                    soupobject = soupobject.find_all(item.split('[')[0], recursive=False)[int(result.groups()[0]) - 1]
+                    soupobject = soupobject.find_all(item.split('[')[0], recursive=False)[
+                        int(result.groups()[0]) - 1]
                 else:
                     soupobject = soupobject.find(item)
             return soupobject.text.strip().strip(u'\u200b')
 
-        scraped_path    =  os.path.join(PATH_REPO, repo_id, PATH_SCRAPED)
-        scraped_file    =  os.listdir(scraped_path)
+        scraped_path = os.path.join(PATH_REPO, repo_id, PATH_SCRAPED)
+        scraped_file = os.listdir(scraped_path)
         parsed_data = []
         for file_count in range(5):
             try:
-                graph = Graph(os.path.join(scraped_path, scraped_file[file_count]))
-                data    =   {'id': scraped_file[file_count].split('.')[0]}
+                graph = Graph(os.path.join(
+                    scraped_path, scraped_file[file_count]))
+                data = {'id': scraped_file[file_count].split('.')[0]}
                 #print('Parsing file {}'.format(scraped_file[file_count]))
                 for row in self.data_table:
                     #print('Soup xpath {}'.format(row))
-                    data[row] = soup_xpath(self.data_table[row], graph.root_node.bs4_node)
+                    data[row] = soup_xpath(
+                        self.data_table[row], graph.root_node.bs4_node)
                 parsed_data.append(data)
             except:
                 pass
