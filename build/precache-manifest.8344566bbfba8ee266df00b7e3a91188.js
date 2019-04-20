8f212f49d0ac2845d422d099382d37b8700bd402
tonny <ton_kriddanai@hotmail.com>
2019-04-20 15:59:29 +0700

    add build

---
+++ build/precache-manifest.8344566bbfba8ee266df00b7e3a91188.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "12e47c6982de54a0956624502d57bf3c",
+    "url": "/static/media/tonny_image.12e47c69.jpg"
+  },
+  {
+    "revision": "18701006e7c756c11d97",
+    "url": "/static/js/main.b2f672f8.chunk.js"
+  },
+  {
+    "revision": "f812c01006a3dac1f9a5abd3146c2336",
+    "url": "/static/media/mongodb-icon.f812c010.png"
+  },
+  {
+    "revision": "5fde3dc6ee9d5d878b6c9a1333a1359e",
+    "url": "/static/media/link-icon.5fde3dc6.png"
+  },
+  {
+    "revision": "2a1bb09ff8132accd754e59df6c78aba",
+    "url": "/static/media/mock.2a1bb09f.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "bd9eedfde865d55351c7d8694c32afa6",
+    "url": "/static/media/under_construction.bd9eedfd.png"
+  },
+  {
+    "revision": "5c7c06fa33ad43dfbe68e717496e4b62",
+    "url": "/static/media/json-icon.5c7c06fa.png"
+  },
+  {
+    "revision": "4db2b83837293451f40b",
+    "url": "/static/js/2.0d5ebcd6.chunk.js"
+  },
+  {
+    "revision": "4db2b83837293451f40b",
+    "url": "/static/css/2.5137106b.chunk.css"
+  },
+  {
+    "revision": "c6e6a51cb4d185c7c90edebfeff877e7",
+    "url": "/index.html"
+  }
+];
--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+python3 -m venv venv
+. venv/bin/activate
+pip install -r requirements.txt
 echo "FLASK IS STARTING"
 ls -l
 cd flask_app
